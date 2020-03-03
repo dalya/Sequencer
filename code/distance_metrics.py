0db64c4c95c5d2d021feb1a5026e1929c9f7b3a9
Dalya Baron <dalyabaron@gmail.com>
2020-03-03 14:29:19 +0200

    changed the documentation of the functions in distance_metrics.py file

--- code/distance_metrics.py
+++ code/distance_metrics.py
@@ -18,21 +18,22 @@ from joblib import Parallel, delayed
 #######################################################################################################################
 
 def return_L2_mat(objects_list, choice_parallelization):
-    """
-    The function returns the Euclidean distance matrix for this dataset. 
-    The L2 distance is symmetric, and the function uses this to reduce the number of computations.
+    """Returns the Euclidean Distance matrix for the list of objects.
+
+    :param objects_list: a list of objects between which the distance will be calculated. Objects are assumed 
+                         be numpy.array
+    :param choice_parallelization: a boolean variable reprensting whether to parallelize the computation
+    :rtype: numpy.array
     """
     n_obj = len(objects_list)
     distance_matrix = numpy.zeros((n_obj, n_obj))
 
     if choice_parallelization == True:
-
         master_index_list = []
         for i in range(n_obj):
             for j in range(i+1, n_obj):
                 master_index_list.append([i,j])
 
-        ## parallelization
         num_cores = multiprocessing.cpu_count()
         results = Parallel(n_jobs=num_cores)(delayed(l2_simple)(objects_list, i) for i in master_index_list)
 
@@ -43,18 +44,23 @@ def return_L2_mat(objects_list, choice_parallelization):
             distance_matrix[j, i] = results[i_pair]
 
     if choice_parallelization == False:
-
         for i in range(n_obj):
             for j in range(i+1, n_obj):
                 obj_i = objects_list[i]
                 obj_j = objects_list[j]
-                distance_matrix[i, j] = numpy.sum((obj_i - obj_j)**2)
-                distance_matrix[j, i] = numpy.sum((obj_i - obj_j)**2)
+                l2_val = numpy.sum((obj_i - obj_j)**2)
+                distance_matrix[i, j] = l2_val
+                distance_matrix[j, i] = l2_val
+
     return distance_matrix
 
 def l2_simple(objects_list, index_pair):
-    """
-    A simple L2 distance measurement to be used with parallelization
+    """Returns the Euclidean Distance between a pair of objects. Function is used during parallelization.
+
+    :param objects_list: a list of objects between which the distance will be calculated. Objects are assumed 
+                         be numpy.array
+    :param index_pair: a list of two indices that correspond to the objects between which the distance is estimated
+    :rtype: numpy.float64
     """
     return numpy.sum((objects_list[index_pair[0]] - objects_list[index_pair[1]])**2)
 
@@ -67,70 +73,50 @@ def l2_simple(objects_list, index_pair):
 #######################################################################################################################
 
 def return_kl_mat(objects_list, choice_parallelization):
-    """
-    The function returns the KL-divergence matrix for this dataset. 
-    Objects should already be normalised and interpolated to the same grid.
-    KL metric is not symmetric, and thus the function performs all the computations.
+    """Returns the KL Divergence matrix for the list of objects.
+
+    :param objects_list: a list of objects between which the distance will be calculated. Objects are assumed 
+                         be numpy.array
+    :param choice_parallelization: a boolean variable reprensting whether to parallelize the computation
+    :rtype: numpy.array
     """
     n_obj = len(objects_list)
     distance_matrix = numpy.zeros((n_obj, n_obj))
 
     if choice_parallelization == True:
-
         master_index_list = []
         for i in range(n_obj):
             for j in range(n_obj):
                 master_index_list.append([i, j])
 
-        ## parallelization
         num_cores = multiprocessing.cpu_count()
-        if len(objects_list[0].shape) == 1:
-            results = Parallel(n_jobs=num_cores)(delayed(kl_simple_1D)(objects_list, i) for i in master_index_list)
-        elif len(objects_list[0].shape) == 2:
-            results = Parallel(n_jobs=num_cores)(delayed(kl_simple_2D)(objects_list, i) for i in master_index_list)
+        results = Parallel(n_jobs=num_cores)(delayed(kl_simple)(objects_list, i) for i in master_index_list)
 
         for i_pair in range(len(results)):
             i = master_index_list[i_pair][0]
             j = master_index_list[i_pair][1]
             distance_matrix[i, j] = results[i_pair]
 
-
     if choice_parallelization == False:
-
-        if len(objects_list[0].shape) == 1:
             for i in range(n_obj):
                 for j in range(n_obj):
-                    obj_i = objects_list[i]
-                    obj_j = objects_list[j]
+                    obj_i = objects_list[i].flatten()
+                    obj_j = objects_list[j].flatten()
                     distance_matrix[i, j] = entropy(obj_i, obj_j)
-
-        elif len(objects_list[0].shape) == 2:
-            for i in range(n_obj):
-                for j in range(n_obj):
-                    obj_i_flat = objects_list[i].flatten()
-                    obj_j_flat = objects_list[j].flatten()
-
-                    distance_matrix[i, j] = entropy(obj_i_flat, obj_j_flat)
 
     return distance_matrix
 
-def kl_simple_1D(objects_list, index_pair):
-    """
-    A simple 1D KL divergence measurement to be used with parallelization
-    """
-    return entropy(objects_list[index_pair[0]], objects_list[index_pair[1]])
+def kl_simple(objects_list, index_pair):
+    """Returns the KL Divergence between a pair of objects. Function is used during parallelization.
 
-def kl_simple_2D(objects_list, index_pair):
+    :param objects_list: a list of objects between which the distance will be calculated. Objects are assumed 
+                         be numpy.array
+    :param index_pair: a list of two indices that correspond to the objects between which the distance is estimated
+    :rtype: numpy.float64
     """
-    A simple 2D KL divergence measurement to be used with parallelization
-    """
-    obj_i = objects_list[index_pair[0]]
-    obj_j = objects_list[index_pair[1]]
-
-    obj_i_flat = obj_i.flatten()
-    obj_j_small = obj_j.flatten()
-
-    return entropy(obj_i_flat, obj_j_flat)
+    obj_i = objects_list[index_pair[0]].flatten()
+    obj_j = objects_list[index_pair[1]].flatten()
+    return entropy(obj_i, obj_j)
 
 #######################################################################################################################
 ##  Earth Mover Distance:                                                                                            ##
@@ -140,23 +126,24 @@ def kl_simple_2D(objects_list, index_pair):
 #######################################################################################################################
 
 def return_emd_mat_brute_force(grid, objects_list, choice_parallelization):
-    """
-    The function returns the EMD distance matrix for this dataset. 
-    The objects are assumed to be normalized and interpolated to a common grid.
-    The EMD distance is symmetric, and the function uses this to reduce the number of computations.
+    """Returns the Earth Mover Distance matrix for the list of objects.
+
+    :param grid: a numpy.array representing the grid on which the objects lie (the x-axis essentially)
+    :param objects_list: a list of objects between which the distance will be calculated. Objects are assumed 
+                         be numpy.array
+    :param choice_parallelization: a boolean variable reprensting whether to parallelize the computation
+    :rtype: numpy.array
     """
     n_obj = len(objects_list)
     n_pix = len(grid)
     distance_matrix = numpy.zeros((n_obj, n_obj))
 
     if choice_parallelization == True:
-
         master_index_list = []
         for i in range(n_obj):
             for j in range(i+1, n_obj):
                 master_index_list.append([i, j])
 
-        ## parallelization
         num_cores = multiprocessing.cpu_count()
         if len(grid.shape) == 1:
             results = Parallel(n_jobs=num_cores)(delayed(emd_simple_1D)(grid, objects_list, i) for i in master_index_list)
@@ -169,11 +156,8 @@ def return_emd_mat_brute_force(grid, objects_list, choice_parallelization):
             distance_matrix[i, j] = results[i_pair]
             distance_matrix[j, i] = results[i_pair]
 
-
     if choice_parallelization == False:
-
         if len(grid.shape) == 1:
-            
             for i in range(n_obj):
                 for j in range(i+1, n_obj):
                     obj_i = objects_list[i]
@@ -200,22 +184,32 @@ def return_emd_mat_brute_force(grid, objects_list, choice_parallelization):
     return distance_matrix
 
 def emd_simple_1D(grid, objects_list, index_pair):
-    """
-    A simple 1D EMD measurement to be used with parallelization
+    """Returns the Earth Mover Distance between a pair of objects. Function is used during parallelization.
+
+    :param grid: a numpy.array representing the grid on which the objects lie (the x-axis essentially)
+    :param objects_list: a list of objects between which the distance will be calculated. Objects are assumed 
+                         be numpy.array
+    :param index_pair: a list of two indices that correspond to the objects between which the distance is estimated
+    :rtype: numpy.float64
     """
     return wasserstein_distance(grid, grid, u_weights=objects_list[index_pair[0]], v_weights=objects_list[index_pair[1]])
 
 def emd_simple_2D(grid, objects_list, index_pair):
-    """
-    A simple 2D EMD measurement to be used with parallelization
+    """Returns the Earth Mover Distance between a pair of objects. Function is used during parallelization.
+
+    :param grid: a numpy.array representing the grid on which the objects lie (the x-axis essentially)
+    :param objects_list: a list of objects between which the distance will be calculated. Objects are assumed 
+                         be numpy.array
+    :param index_pair: a list of two indices that correspond to the objects between which the distance is estimated
+    :rtype: numpy.float64
     """
     obj_i = objects_list[index_pair[0]]
     obj_j = objects_list[index_pair[1]]
-
     X = numpy.column_stack(numpy.nonzero(obj_i))
     Y = numpy.column_stack(numpy.nonzero(obj_j))
     X_w = obj_i[numpy.nonzero(obj_i)] 
     Y_w = obj_j[numpy.nonzero(obj_j)]
+
     return emd(X, Y, X_weights=X_w, Y_weights=Y_w)
 
 #######################################################################################################################
@@ -225,10 +219,13 @@ def emd_simple_2D(grid, objects_list, index_pair):
 #######################################################################################################################
 
 def return_energy_mat(grid, objects_list, choice_parallelization):
-    """
-    The function returns the energy distance matrix for this dataset. 
-    The objects are assumed to be normalized and interpolated to a common grid.
-    The energy distance is symmetric, and the function uses this to reduce the number of computations.
+    """Returns the Energy Distance matrix for the list of objects.
+
+    :param grid: a numpy.array representing the grid on which the objects lie (the x-axis essentially)
+    :param objects_list: a list of objects between which the distance will be calculated. Objects are assumed 
+                         be numpy.array
+    :param choice_parallelization: a boolean variable reprensting whether to parallelize the computation
+    :rtype: numpy.array
     """
     assert len(grid.shape) == 1, "energy distance can only be applied on 1D objects"
 
@@ -237,13 +234,11 @@ def return_energy_mat(grid, objects_list, choice_parallelization):
     distance_matrix = numpy.zeros((n_obj, n_obj))
 
     if choice_parallelization == True:
-
         master_index_list = []
         for i in range(n_obj):
             for j in range(i+1, n_obj):
                 master_index_list.append([i, j])
 
-        ## parallelization
         num_cores = multiprocessing.cpu_count()
         results = Parallel(n_jobs=num_cores)(delayed(energy_simple_1D)(grid, objects_list, i) for i in master_index_list)
 
@@ -253,21 +248,24 @@ def return_energy_mat(grid, objects_list, choice_parallelization):
             distance_matrix[i, j] = results[i_pair]
             distance_matrix[j, i] = results[i_pair]
 
-
     if choice_parallelization == False:
-            
         for i in range(n_obj):
             for j in range(i+1, n_obj):
                 obj_i = objects_list[i]
                 obj_j = objects_list[j]
-                emd_val = energy_distance(grid, grid, u_weights=obj_i, v_weights=obj_j)
-                distance_matrix[i, j] = emd_val
-                distance_matrix[j, i] = emd_val
+                energy_val = energy_distance(grid, grid, u_weights=obj_i, v_weights=obj_j)
+                distance_matrix[i, j] = energy_val
+                distance_matrix[j, i] = energy_val
 
     return distance_matrix
 
 def energy_simple_1D(grid, objects_list, index_pair):
-    """
-    A simple 1D energy distance measurement to be used with parallelization
+    """Returns the Earth Mover Distance between a pair of objects. Function is used during parallelization.
+
+    :param grid: a numpy.array representing the grid on which the objects lie (the x-axis essentially)
+    :param objects_list: a list of objects between which the distance will be calculated. Objects are assumed 
+                         be numpy.array
+    :param index_pair: a list of two indices that correspond to the objects between which the distance is estimated
+    :rtype: numpy.float64
     """
     return energy_distance(grid, grid, u_weights=objects_list[index_pair[0]], v_weights=objects_list[index_pair[1]])
