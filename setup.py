a2c947443136797a13605288cdec369f917809b0
Dalya Baron <dalyabaron@gmail.com>
2020-03-16 10:33:32 +0200

    finished editing the setup.py file

--- setup.py
+++ setup.py
@@ -4,19 +4,36 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="Sequencer",
+    name="TheSequencer",
     version="0.0.1",
-    author="Dalya Baron",
-    author_email="dalyabaron@gmail.com",
     description="An algorithm that detects one-dimensional sequences in complex datasets",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
-    packages=setuptools.find_packages(),
     classifiers=[
-        "Programming Language :: Python :: 3",
+        "Intended Audience :: Science/Research",
+        "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Topic :: Software Development",
+        "Topic :: Scientific/Engineering",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: POSIX",
+        "Operating System :: Unix",
+        "Operating System :: MacOS",
+    ],
+    keywords="dimensionality reduction t-sne UMAP",
+    url="https://github.com/dalya/Sequencer",
+    maintainer="Dalya Baron",
+    maintainer_email="dalyabaron@gmail.com",
+    license="BSD",
+    packages=setuptools.find_packages(),
+
+    install_requires=[
+        "numpy >= 1.13",
+        "scikit-learn >= 0.20",
+        "scipy >= 1.0",
+        "networkx >= 2.4",
+        "joblib >= 0.13.2",
     ],
     python_requires='>=3.6',
 )
